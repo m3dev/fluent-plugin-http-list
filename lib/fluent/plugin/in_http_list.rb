5332f66a2d9350f102ec9bc587f451c039ac855b
Paul McCann <p-mccann@m3.com>
2013-01-10 17:10:42 +0900

    Move copyright info to LICENSE file. -POLM

--- lib/fluent/plugin/in_http_list.rb
+++ lib/fluent/plugin/in_http_list.rb
@@ -1,22 +1,3 @@
-#
-# Fluent HTTP List plugin
-#
-# Copyright 2013 M3, Inc.
-# Written by Paul McCann (p-mccann@m3.com)
-# Based on the HTTP Input Plugin by FURUHASHI Sadayuki included with Fluentd
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-##
 module Fluent
 
 class HttpListInput < Input
